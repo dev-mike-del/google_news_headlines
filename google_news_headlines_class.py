3bbc6450e7aad82ee8e3d9eba5386882396b5174
Michael Delgado <devmikedel@gmail.com>
2020-04-14 21:28:54 -0500

    Add as_dict method

--- google_news_headlines_class.py
+++ google_news_headlines_class.py
@@ -1,11 +1,49 @@
-from google_news_headlines_function imoprt google_news_headlines 
+#!/usr/bin/env python3
+
+from datetime import datetime, timezone
+
+# Beautiful Soup is a Python library for pulling data out of HTML and XML files.
+from bs4 import BeautifulSoup
+
+# Requests is a Python HTTP library
+import requests
+
+# lxml is a Python library which allows for easy handling of XML and HTML files.
+import lxml
 
 
 class GoogleNewsHeadlines(object):
-	"""docstring for GoogleNewsHeadlines"""
-	def __init__(self, arg):
-		super(GoogleNewsHeadlines, self).__init__()
-		self.arg = arg
+    """docstring for GoogleNewsHeadlines"""
+    def __init__(self):
+        super(GoogleNewsHeadlines, self).__init__()
+        r = requests.get('https://news.google.com/')
+        soup = BeautifulSoup(r.text, 'lxml')
+        self.source = soup.findAll(True, {'class':['DY5T1d', 'wEwyrc']})
+        self.timestamp = datetime.now(timezone.utc)
 
-	def function(self):
-		pass
+
+    def _as_dict(self):
+        results = {}
+        index = 0
+        iter_source =  iter(self.source)
+
+        for headlines in iter_source:
+            org = next(iter_source)
+            results[index] = (self.timestamp, headlines.text, org.text)
+            index += 1
+
+        return results
+
+
+    def word_count(self):
+        return self._as_dict()
+
+
+def main():
+    data = GoogleNewsHeadlines()
+    print(data._as_dict())
+
+
+if __name__ == '__main__':
+    main()
+
