9d1e75fc8c7fdb0ae3e49c21c73bc77418a909b7
Michael Delgado <devmikedel@gmail.com>
2020-04-14 19:17:46 -0500

    Add GoogleNewsHeadlines class to google_news_headlines_class.py

---
+++ google_news_headlines_class.py
@@ -0,0 +1,11 @@
+from google_news_headlines_function imoprt google_news_headlines 
+
+
+class GoogleNewsHeadlines(object):
+	"""docstring for GoogleNewsHeadlines"""
+	def __init__(self, arg):
+		super(GoogleNewsHeadlines, self).__init__()
+		self.arg = arg
+
+	def function(self):
+		pass
